efc7134a4075c2e89939305d4344be8fa013b0f1
Rueeda <rueeda.1940@gmail.com>
2024-07-31 13:20:31 +0300

    Solution

--- calculator.py
+++ calculator.py
@@ -1,26 +1,47 @@
+from calculator import add 
+
 def add(x, y):
     return x + y
-
+result = add(1,2) 
+print(result) 
 
 def subtract(x, y):
-    pass
+    return x - y
+result = subtract (10,7)
+print ( result )
 
 
 def divide(x, y):
-    pass
+    return x / y
+result = divide (6,2)
+print( result)
 
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero"
+    return x / y
+
+def divide_by_zero():
+    result = divide(99, 0)
+    print(type(result))
+divide_by_zero()
 
 def multiply(x, y):
-    pass
-
+    return x * y
+result = multiply (2,3)
+print ( result )
 
 def square(x):
-    pass
-
+    return x ** 2
+result = square (3)
+print ( result)
 
 def power(x, y):
-    pass
-
+   return x ** y
+result = power(3,2)
+print (result)
 
 def sqrt(x):
-    pass
+    return x ** 0.5
+result = sqrt(9)
+print (result)
